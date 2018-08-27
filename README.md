d784af9e06476cb9851a1e110782f16f7ad122db
livelybone <2631541504@qq.com>
2018-08-27 12:00:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# @livelybone/meta-scale
+A module for get meta-scale
+
+## repository
+https://github.com/livelybone/meta-scale.git
+
+## Demo
+https://github.com/livelybone/meta-scale#readme
+
+## TODO
---
+++ src/index.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+  var viewport = Array.prototype.filter.call(document.getElementsByTagName('meta'), function (meta) {
+    return meta.getAttribute('name') === 'viewport'
+  })
+  if (!viewport) return 1
+  viewport = viewport[viewport.length - 1]
+  var contentStr = viewport.getAttribute('content')
+  var reg = /initial-scale\s*=\s*([^,]+)(,|$)/
+  var arr = contentStr.match(reg)
+  var scale = arr ? +arr[1] : 1
+  return scale || 1
+}
